a37e47a64cc205cc8fc8e84eaf5ea7d3b1b1d5c5
manishsxtha <manish.xshrestha@gmail.com>
2019-11-26 19:40:26 +0545

    faster loading of illustration detail fragment during swipes

--- app/src/main/java/com/perol/asdpl/pixivez/fragments/PictureXFragment.kt
+++ app/src/main/java/com/perol/asdpl/pixivez/fragments/PictureXFragment.kt
@@ -46,7 +46,6 @@ import com.perol.asdpl.pixivez.adapters.PictureXAdapter
 import com.perol.asdpl.pixivez.adapters.TagsAdapter
 import com.perol.asdpl.pixivez.databinding.FragmentPictureXBinding
 import com.perol.asdpl.pixivez.dialog.CommentDialog
-import com.perol.asdpl.pixivez.objects.LazyV4Fragment
 import com.perol.asdpl.pixivez.objects.Toasty
 import com.perol.asdpl.pixivez.services.GlideApp
 import com.perol.asdpl.pixivez.viewmodel.PictureXViewModel
@@ -64,11 +63,11 @@ private const val ARG_PARAM1 = "param1"
  * create an instance of this fragment.
  *
  */
-class PictureXFragment : LazyV4Fragment() {
+class PictureXFragment : Fragment() {
 
     private var param1: Long? = null
     lateinit var pictureXViewModel: PictureXViewModel
-    override fun loadData() {
+    fun loadData() {
 //        val item = activity?.intent?.extras
 //        val illust = item?.getParcelable<Illust>(param1.toString())
         pictureXViewModel.firstGet(param1!!, null)
